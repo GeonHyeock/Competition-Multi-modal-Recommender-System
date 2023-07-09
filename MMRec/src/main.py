7279beb35c80045b35c8bc11801e0cdecfe82eac
GeonHyeock <gunheock@gmail.com>
2023-07-09 22:26:56 +0900

    Feat : Baseline BM3

--- MMRec/src/main.py
+++ MMRec/src/main.py
@@ -9,21 +9,30 @@ Main entry
 
 import os
 import argparse
+import yaml
 from utils.quick_start import quick_start
-os.environ['NUMEXPR_MAX_THREADS'] = '48'
+
+os.environ["NUMEXPR_MAX_THREADS"] = "48"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', type=str, default='FREEDOM', help='name of models')
-    parser.add_argument('--dataset', '-d', type=str, default='baby', help='name of datasets')
+    parser.add_argument("--model", "-m", type=str, default="BM3", help="name of models")
+    parser.add_argument(
+        "--dataset", "-d", type=str, default="Inha", help="name of datasets"
+    )
 
-    config_dict = {
-        'gpu_id': 0,
-    }
+    with open("./src/configs/overall.yaml") as f:
+        config_dict = yaml.load(f, Loader=yaml.FullLoader)
 
     args, _ = parser.parse_known_args()
 
-    quick_start(model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True)
+    with open(f"./src/configs/model/{args.model}.yaml") as f:
+        config_dict.update(yaml.load(f, Loader=yaml.FullLoader))
 
+    with open(f"./src/configs/dataset/{args.dataset}.yaml") as f:
+        config_dict.update(yaml.load(f, Loader=yaml.FullLoader))
 
+    quick_start(
+        model=args.model, dataset=args.dataset, config_dict=config_dict, save_model=True
+    )
