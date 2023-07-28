6d630a95077c7b0e617c2c776329fcc89a59b8c0
GeonHyeock <gunheock@gmail.com>
2023-07-28 13:12:09 +0900

    Feat : weighted voting

--- src/ensemble.py
+++ src/ensemble.py
@@ -32,8 +32,30 @@ def hard_voting(output_file_list, name="result"):
     result.to_csv(f"./{name}.csv", index=False)
 
 
+def weighted_voting(output_file_list, name="weight_ensemble", item_num=63001):
+    result = []
+    for file in output_file_list:
+        data = pd.read_csv(file)
+        data = data.groupby("user_id").apply(lambda x: x.item_id.values).values
+        result.append(np.array([*data]))
+
+    result, user_id, item_id = np.array(result).swapaxes(0, 1), [], []
+    for user in tqdm(range(result.shape[0])):
+        data, item = result[user, :, :], np.zeros(item_num)
+        for i in range(50):
+            for idx in data[:, i]:
+                item[idx] += 1 + 1 / np.log2(i + 2)
+
+        user_id.extend(np.repeat(user, 50))
+        item_id.extend(np.argsort(item)[::-1][:50])
+
+    result = pd.DataFrame({"user_id": user_id, "item_id": item_id})
+    result.to_csv(f"./{name}.csv", index=False)
+
+
 if __name__ == "__main__":
     os.chdir("submission")
     output_file_list = glob("./BM3/*.csv")
     output_file_list = list(filter(lambda x: "inter" in x, output_file_list))
-    hard_voting(output_file_list, "fold_ensemble")
+    # hard_voting(output_file_list, "fold_ensemble")
+    weighted_voting(output_file_list)
