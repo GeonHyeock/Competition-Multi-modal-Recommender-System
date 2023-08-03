ae17fa92203402f54737cb9853e49b4ecfce2549
GeonHyeock <gunheock@gmail.com>
2023-08-04 06:25:13 +0900

    Feat : wandb

--- NCL/trainer.py
+++ NCL/trainer.py
@@ -5,17 +5,25 @@ from tqdm import tqdm
 
 from recbole.trainer import Trainer
 from recbole.utils import early_stopping, dict2str, set_color, get_gpu_usage
+import wandb
 
 
 class NCLTrainer(Trainer):
-
     def __init__(self, config, model):
         super(NCLTrainer, self).__init__(config, model)
 
-        self.num_m_step = config['m_step']
+        self.num_m_step = config["m_step"]
         assert self.num_m_step is not None
 
-    def fit(self, train_data, valid_data=None, verbose=True, saved=True, show_progress=False, callback_fn=None):
+    def fit(
+        self,
+        train_data,
+        valid_data=None,
+        verbose=True,
+        saved=True,
+        show_progress=False,
+        callback_fn=None,
+    ):
         r"""Train the model based on the train data and the valid data.
 
         Args:
@@ -37,54 +45,87 @@ class NCLTrainer(Trainer):
         self.eval_collector.data_collect(train_data)
 
         for epoch_idx in range(self.start_epoch, self.epochs):
-
             # only differences from the original trainer
             if epoch_idx % self.num_m_step == 0:
                 self.logger.info("Running E-step ! ")
                 self.model.e_step()
             # train
             training_start_time = time()
-            train_loss = self._train_epoch(train_data, epoch_idx, show_progress=show_progress)
-            self.train_loss_dict[epoch_idx] = sum(train_loss) if isinstance(train_loss, tuple) else train_loss
+            train_loss = self._train_epoch(
+                train_data, epoch_idx, show_progress=show_progress
+            )
+            self.train_loss_dict[epoch_idx] = (
+                sum(train_loss) if isinstance(train_loss, tuple) else train_loss
+            )
             training_end_time = time()
-            train_loss_output = \
-                self._generate_train_loss_output(epoch_idx, training_start_time, training_end_time, train_loss)
+            train_loss_output = self._generate_train_loss_output(
+                epoch_idx, training_start_time, training_end_time, train_loss
+            )
             if verbose:
                 self.logger.info(train_loss_output)
-            self._add_train_loss_to_tensorboard(epoch_idx, train_loss)
+            wandb.log(
+                {
+                    "epoch": epoch_idx,
+                    "train/loss_1": train_loss[0],
+                    "train/loss_2": train_loss[1],
+                }
+            )
 
             # eval
             if self.eval_step <= 0 or not valid_data:
                 if saved:
                     self._save_checkpoint(epoch_idx)
-                    update_output = set_color('Saving current', 'blue') + ': %s' % self.saved_model_file
+                    update_output = (
+                        set_color("Saving current", "blue")
+                        + ": %s" % self.saved_model_file
+                    )
                     if verbose:
                         self.logger.info(update_output)
                 continue
             if (epoch_idx + 1) % self.eval_step == 0:
                 valid_start_time = time()
-                valid_score, valid_result = self._valid_epoch(valid_data, show_progress=show_progress)
-                self.best_valid_score, self.cur_step, stop_flag, update_flag = early_stopping(
+                valid_score, valid_result = self._valid_epoch(
+                    valid_data, show_progress=show_progress
+                )
+                (
+                    self.best_valid_score,
+                    self.cur_step,
+                    stop_flag,
+                    update_flag,
+                ) = early_stopping(
                     valid_score,
                     self.best_valid_score,
                     self.cur_step,
                     max_step=self.stopping_step,
-                    bigger=self.valid_metric_bigger
+                    bigger=self.valid_metric_bigger,
                 )
                 valid_end_time = time()
-                valid_score_output = (set_color("epoch %d evaluating", 'green') + " [" + set_color("time", 'blue')
-                                    + ": %.2fs, " + set_color("valid_score", 'blue') + ": %f]") % \
-                                     (epoch_idx, valid_end_time - valid_start_time, valid_score)
-                valid_result_output = set_color('valid result', 'blue') + ': \n' + dict2str(valid_result)
+                valid_score_output = (
+                    set_color("epoch %d evaluating", "green")
+                    + " ["
+                    + set_color("time", "blue")
+                    + ": %.2fs, "
+                    + set_color("valid_score", "blue")
+                    + ": %f]"
+                ) % (epoch_idx, valid_end_time - valid_start_time, valid_score)
+                valid_result_output = (
+                    set_color("valid result", "blue") + ": \n" + dict2str(valid_result)
+                )
                 if verbose:
                     self.logger.info(valid_score_output)
                     self.logger.info(valid_result_output)
-                self.tensorboard.add_scalar('Vaild_score', valid_score, epoch_idx)
+                gh_val = list(
+                    filter(lambda x: len(x) >= 2, dict2str(valid_result).split(" "))
+                )
+                wandb.log({i: float(j) for i, j in zip(gh_val[0::2], gh_val[1::2])})
 
                 if update_flag:
                     if saved:
                         self._save_checkpoint(epoch_idx)
-                        update_output = set_color('Saving current best', 'blue') + ': %s' % self.saved_model_file
+                        update_output = (
+                            set_color("Saving current best", "blue")
+                            + ": %s" % self.saved_model_file
+                        )
                         if verbose:
                             self.logger.info(update_output)
                     self.best_valid_result = valid_result
@@ -93,8 +134,9 @@ class NCLTrainer(Trainer):
                     callback_fn(epoch_idx, valid_score)
 
                 if stop_flag:
-                    stop_output = 'Finished training, best eval result in epoch %d' % \
-                                  (epoch_idx - self.cur_step * self.eval_step)
+                    stop_output = "Finished training, best eval result in epoch %d" % (
+                        epoch_idx - self.cur_step * self.eval_step
+                    )
                     if verbose:
                         self.logger.info(stop_output)
                     break
@@ -124,27 +166,37 @@ class NCLTrainer(Trainer):
                 train_data,
                 total=len(train_data),
                 ncols=100,
-                desc=set_color(f"Train {epoch_idx:>5}", 'pink'),
-            ) if show_progress else train_data
+                desc=set_color(f"Train {epoch_idx:>5}", "pink"),
+            )
+            if show_progress
+            else train_data
         )
         for batch_idx, interaction in enumerate(iter_data):
             interaction = interaction.to(self.device)
             self.optimizer.zero_grad()
             losses = loss_func(interaction)
             if isinstance(losses, tuple):
-                if epoch_idx < self.config['warm_up_step']:
+                if epoch_idx < self.config["warm_up_step"]:
                     losses = losses[:-1]
                 loss = sum(losses)
                 loss_tuple = tuple(per_loss.item() for per_loss in losses)
-                total_loss = loss_tuple if total_loss is None else tuple(map(sum, zip(total_loss, loss_tuple)))
+                total_loss = (
+                    loss_tuple
+                    if total_loss is None
+                    else tuple(map(sum, zip(total_loss, loss_tuple)))
+                )
             else:
                 loss = losses
-                total_loss = losses.item() if total_loss is None else total_loss + losses.item()
+                total_loss = (
+                    losses.item() if total_loss is None else total_loss + losses.item()
+                )
             self._check_nan(loss)
             loss.backward()
             if self.clip_grad_norm:
                 clip_grad_norm_(self.model.parameters(), **self.clip_grad_norm)
             self.optimizer.step()
             if self.gpu_available and show_progress:
-                iter_data.set_postfix_str(set_color('GPU RAM: ' + get_gpu_usage(self.device), 'yellow'))
+                iter_data.set_postfix_str(
+                    set_color("GPU RAM: " + get_gpu_usage(self.device), "yellow")
+                )
         return total_loss
